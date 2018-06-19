e11402e2904a54a540fb8cbacc1ceb66c02542f8
Antonio Borrillo <antonio.borrillo@lhasalimited.org>
2018-06-19 10:57:12 +0100

    Angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import { ProductListComponent } from './products/product-list.component';
+
 
 import { AppComponent } from './app.component';
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star.component";
-import { ProductDetailComponent } from './products/product-detail.component';
+
 import {WelcomeComponent} from "./home/welcome.component";
 import {RouterModule} from "@angular/router";
-import { ProductGuardService } from './products/product-guard.service';
+
+import { ProductModule } from './products/product.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
+
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductGuardService],//list of guards when activating product detail route
-        component: ProductDetailComponent },
+
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full'},
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductModule
   ],
-  providers: [ProductGuardService],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
